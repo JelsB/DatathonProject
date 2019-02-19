a93d3e0a42f5ea7534099b4acc7cd5db476933cc
Karan Dsilva <karansinghd@alsephina.ster.kuleuven.be>
2019-02-19 20:29:31 +0100

    played with the dataframe.plot

--- pandas_input.py
+++ pandas_input.py
@@ -1,27 +1,42 @@
 import pandas as pd
 from pathlib import Path
+import matplotlib
+#This is only when using python3 with Mac
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 import json
 ##########################################################
 # with json_file.open('r') as f:
 #         data_dict = json.load(f)
 #         print('Done reading in')
 ###########################################################
-#work with only one year of data (for now)
-year = 1989
+
+
 #Path to the data
 filename = Path('./data/UN/un-general-debates.csv')
-#Load in the data and print the first 5 lines
+#Load in the data and print the column names
 data = pd.read_csv(filename)
-# print(data.head())
+print(data.columns)
 
-#data['country']
-#These two are equivalent. The first part also allows row slicing
+#work with only one year of data (for now) or country (for example India)
+year = 1989
+country = 'IND'
+
+#Two ways to slice by column name:
+#The first one also allows row slicing
 # print(data.ix[:,'year'])
-# print('###############')
+#The second is easier to read and gives us the same result
 # print(data['year'])
 
 #Filter data from the year we want to work with:
-year_indices = data['year']==year
+year_indices = data.year==year
+country_indices = data.country==country
 #data['year_indices'] gives us the data frame with entries of the year we want.
 #we can further obtain the countries that participated that year with
-print(data[year_indices]['country'])
+# countries = data[year_indices].country.values
+# print(len(countries))
+years_array = data.year[country_indices]
+print(years_array.values)
+data['country'].value_counts().plot(kind='bar')
+plt.show()
+# data.plot(y=countries,kind='bar')
