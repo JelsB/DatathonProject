99a69977f9233d8aa7cf0b539c4515cf104fff8e
Karan Dsilva <karansinghd@alsephina.ster.kuleuven.be>
2019-02-20 23:33:55 +0100

    Dictionary added

--- pandas_input.py
+++ pandas_input.py
@@ -1,16 +1,22 @@
 import pandas as pd
+import numpy as np
 from pathlib import Path
 import matplotlib
 #This is only when using python3 with Mac
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import json
+import pickle
+
+
 ##########################################################
 #snipped to open a .json file (country abbreviations)
 # with json_file.open('r') as f:
 #         data_dict = json.load(f)
 #         print('Done reading in')
 ###########################################################
+#G20 countries
+g20_countries=['ARG']#,'AUS','BRA','CAN','CHN']
 #define a function to search for a keyword and return the year and country-wise
 #counts
 def search_keyword(data,keyword):
@@ -19,9 +25,12 @@ def search_keyword(data,keyword):
     data_of_interest = data[indices]
     # data_of_interest.country.value_counts().plot(kind='bar')
     # data_of_interest.plot(x='year',y='country',kind='bar',subplots=True,sharex=True)
-    data_of_interest.year.value_counts(ascending=True).plot(kind='bar')
-    plt.show()
-    return None
+    # data_of_interest.year.value_counts(ascending=True).plot(kind='bar')
+    for g20 in g20_countries:
+        data_of_interest=data_of_interest[data_of_interest.country==g20]
+        data_of_interest.year.value_counts().plot(kind='bar',title=g20,rot=-90)
+        plt.show()
+
 
 #Path to the data
 filename = Path('./data/UN/un-general-debates.csv')
@@ -47,24 +56,35 @@ print(f'during the years {dataset.year.min()} to {dataset.year.max()}')
 year_indices = dataset.year==year_of_interest
 country_indices = dataset.country==country_of_interest
 
+
+# load in country code dictionary:
+with open('members_dic.pkl', 'rb') as pkl_file:
+    country_dic = pickle.load(pkl_file)
+
+'''
+Yo Karan, did this work for you?
+'''
+
+
 #data['year_indices'] gives us the data frame with entries of the year we want.
 #we can further obtain the countries that participated that year with
 # countries = data.country[year_indices].values
 #if we do not use the '.values', then we get a recarray which has a dtype='object'
 # print(len(countries))
 years_array = dataset.year[country_indices]
-print(f'List of years when {country_of_interest} has spoken')
-print(years_array.values)
+print(f'Country of interest: {country_dic[country_of_interest]}')
+print(f'List of years when {country_dic[country_of_interest]} has spoken')
+print(years_array.sort_values().values)
 # dataset['country'].value_counts().plot(kind='bar')
 #To find the indices where IND occurs in the country list (just as an example,
 #this is meant to be used in the text column). Returns -1 where not found.
 #Follows default indexing, but can be changed by giving a 'start' value.
-#Verdict: str.find() does something weird, use str.contains()
+#VERDICT: str.find() does something weird, use str.contains()
 # indices_country_find = data.country.str.find('IN')
 # print(data.country.values[indices_country_find])
 print('Using the search function "contains"')
-indices_country_contains = dataset.country.str.contains('IND')
-# print(dataset.country.values[indices_country_contains])
+indices_country_contains = dataset.country.str.contains('CHN')
+print(dataset.country.values[indices_country_contains])
 
 #now to try and apply this to the text column
 text_contents = dataset.text[year_indices]
@@ -72,5 +92,5 @@ text_contents = dataset.text[year_indices]
 # data.plot(y=countries,kind='bar')
 search_keyword(dataset,"war")
 #get a unique list of countries and get yearly data country by country
-# unique_countries = data.country.unique()
+# unique_countries = dataset.country.unique()
 # for c in unique_countries:
