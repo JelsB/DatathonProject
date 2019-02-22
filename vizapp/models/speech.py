c0803d727f4c129f3030f688455f4bf7deaf1d70
Julia Bodensteiner <julia.bodensteiner@kuleuven.be>
2019-02-22 23:27:23 +0100

    Improved map plotting

--- vizapp/models/speech.py
+++ vizapp/models/speech.py
@@ -1,12 +1,13 @@
 from math import pi
 from collections import Counter
-from bokeh.layouts import widgetbox, row
+from bokeh.layouts import widgetbox, row, column
 from bokeh.models.widgets import TextInput, MultiSelect, Toggle, CheckboxGroup
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, Panel, Range1d
 from bokeh.palettes import Category20c, Category20_16
 from bokeh.transform import cumsum
-from bokeh.events import ButtonClick
+from bokeh.models import LogColorMapper
+from bokeh.palettes import Viridis6 as palette
 
 # from .utils import country_dic
 from .utils import country_dic
@@ -71,8 +72,10 @@ def speech_tab(list_of_sp_obj):
                 'labels': labels}
 
         country_data = make_pie_data(country_counter)
+        prepared_map_data = make_map_data(country_counter)
 
-        return ColumnDataSource(data), ColumnDataSource(country_data)
+        return (ColumnDataSource(data), ColumnDataSource(country_data),
+                ColumnDataSource(prepared_map_data))
 
     def make_pie_data(country_counter, max_countries=15):
         if max_countries > 19:
@@ -94,14 +97,48 @@ def speech_tab(list_of_sp_obj):
         total_counts = sum(data['counts'])
         data['angle'] = [i/total_counts*2*pi for i in data['counts']]
         data['color'] = Category20c[len(data['country'])]
-
         return data
+
+    def make_map_data(country_counter):
+        most_common = country_counter.most_common(500)
+        unzipped = list(zip(*most_common))
+        countries = list(unzipped[0])
+        country_counts = list(unzipped[1])
+
+        data = dict()
+        data['country'] = countries
+        data['counts'] = country_counts
+
+        k = list(country_shapes.keys())
+        country_xs = [country_shapes[i]['lats'] for i in k]
+        country_ys = [country_shapes[i]['lons'] for i in k]
+        country_names = [country_shapes[i]['name'] for i in k]
+        # country_rates = list(range(len(country_names)))
+
+        country_rates = [float('NaN')]*len(country_names)
+        country_inds = {country_shapes[j]['ID']: i for i, j in enumerate(k)}
+
+        for i in range(len(data['country'])):
+            try:
+                country_rates[country_inds[data['country'][i]]] = data['counts'][i]
+            except:
+                pass
+
+        src_map = dict(
+            x=country_xs,
+            y=country_ys,
+            name=country_names,
+            rate=country_rates,
+        )
+
+        return src_map
 
     def make_plot(src, selected_countries):
         p = figure(plot_width=400, plot_height=400)
         # print('SRC', src['years'], src['counts'])
         # print(src.daa['labels'])
-        p.multi_line('years', 'counts', color='colors', legend='labels', source=src)
+        p.multi_line('years', 'counts', color='colors', legend='labels',
+                     source=src)
         #
         # print(selected_countries)
         # for country in selected_countries:
@@ -111,13 +148,15 @@ def speech_tab(list_of_sp_obj):
 
     def make_pie_plot(src):
 
-        p = figure(plot_width=400, plot_height=400, title="Pie Chart", toolbar_location=None,
-                   tools="hover", tooltips="@country: @counts", x_range=(-0.5, 1.0))
+        p = figure(plot_width=400, plot_height=400, title="Pie Chart",
+                   toolbar_location=None, tools="hover",
+                   tooltips="@country: @counts", x_range=(-0.5, 1.0))
 
         p.wedge(x=0, y=1, radius=0.4,
                 start_angle=cumsum('angle', include_zero=True),
                 end_angle=cumsum('angle'),
-                line_color="white", fill_color='color', legend='country', source=src)
+                line_color="white", fill_color='color',
+                legend='country', source=src)
 
         p.axis.axis_label = None
         p.axis.visible = False
@@ -125,29 +164,41 @@ def speech_tab(list_of_sp_obj):
 
         return p
 
-    def make_map():
-        tools = "pan,wheel_zoom,box_zoom,reset, hover"
+    def make_map(src_map):
+        color_mapper = LogColorMapper(palette=palette)
+        TOOLS = "pan,wheel_zoom,reset,hover,save"
 
-        p = figure(width=1000, height=600, tools=tools,
-                   title='World Countries', x_axis_label='Longitude',
-                   y_axis_label='Latitude')
-        p.background_fill_color = "midnightblue"
+        p = figure(plot_width=1150, plot_height=800,
+                   title="World map", tools=TOOLS,
+                   x_axis_location=None, y_axis_location=None,
+                   tooltips=[
+                       ("Name", "@name"), ("Mentions", "@rate")  # ,
+                       # ("(Long, Lat)", "($x, $y)")
+                   ])
+        p.grid.grid_line_color = None
+        p.hover.point_policy = "follow_mouse"
         p.x_range = Range1d(start=-180, end=180)
         p.y_range = Range1d(start=-90, end=90)
-        p.patches("xs", "ys", color="white", line_color="black",
-                  source=country_shapes)
-        return p
+
+        p.grid.grid_line_color = None
+
+        p.patches('x', 'y', source=src_map,
+                  fill_color={'field': 'rate', 'transform': color_mapper},
+                  fill_alpha=0.7, line_color="white", line_width=0.5)
+
+        return(p)
 
     def update(attr, old, new):
         print('updating', multi_select.value)
         (word_frequency_to_plot,
-         pie_src_new) = make_data_set(list_of_sp_obj,
-                                      text_input.value,
-                                      multi_select.value,
-                                      total_box.active)
+         pie_src_new, map_src_new) = make_data_set(list_of_sp_obj,
+                                                   text_input.value,
+                                                   multi_select.value,
+                                                   total_box.active)
         # print(text_input.value, word_frequency_to_plot)
         src.data.update(word_frequency_to_plot.data)
         pie_src.data.update(pie_src_new.data)
+        map_src.data.update(map_src_new.data)
 
     word_colors = Category20_16
     word_colors.sort()
@@ -163,18 +214,18 @@ def speech_tab(list_of_sp_obj):
     total_box = CheckboxGroup(labels=['Show Total'], active=[0, 1])
     total_box.on_change('active', update)
 
-    src, pie_src = make_data_set(list_of_sp_obj, text_input.value,
-                                 multi_select.value, total_box.active)
+    src, pie_src, map_src = make_data_set(list_of_sp_obj, text_input.value,
+                                          multi_select.value, total_box.active)
 
     p = make_plot(src, multi_select.value)
     pie = make_pie_plot(pie_src)
-    map = make_map()
+    map = make_map(map_src)
 
     # Put controls in a single element
     controls = widgetbox(text_input, total_box, multi_select)
 
     # Create a row layout
-    layout = row(controls, p, pie, map)
+    layout = row(controls, column(p, pie), map)
 
     # Make a tab with the layout
     tab = Panel(child=layout, title='Word frequency')
