84250725058c5e7d8fe15464bbddf2c32334282a
JelsB <boulangier.jels@gmail.com>
2019-05-19 20:27:27 +0200

    MAIN: Tidy-up code

--- vizapp/main.py
+++ vizapp/main.py
@@ -1,11 +1,6 @@
-# Pandas for data management
-import pandas as pd
-
-# os methods for manipulating paths
-from pathlib import Path
-
-# pickle for the dictionary
 import pickle
+import pandas as pd
+from pathlib import Path
 
 # Bokeh basics
 from bokeh.io import curdoc
@@ -13,54 +8,29 @@ from bokeh.models.widgets import Tabs
 from bokeh.themes import built_in_themes
 
 # Each tab is drawn by one script
-from models.make_speeches import make_speeches, pickle_speeches, unpickle_speeches
 from models.speech import speech_tab
-# from models.country import create_dict
 from models.full_text import text_tab
 from models.country import country_tab
+from models.make_speeches import (make_speeches, pickle_list_of_speeches,
+                                  unpickle_list_of_speeches)
 
-
-# Using included state data from Bokeh for map
-from bokeh.sampledata.us_states import data as states
-
-
+# Read data for first time
 # Read data into dataframes
 # filename = Path('./data/UN/un-general-debates.csv')
-# Load in the data and print the column names
 # dataset = pd.read_csv(filename)
-# raw_speeches = dataset.text
-# sample_raw_speeches = raw_speeches[:100]
-# sample_dataset = dataset[:]
-# print(raw_speeches)
-# flights = pd.read_csv(join(dirname(__file__), 'data', 'flights.csv'),
-# 	                                          index_col=0).dropna()
+# speech_objects = make_speeches(dataset)
+# pickle_list_of_speeches(speech_objects, './data/')
 
-# load in country code dictionary:
-# with open('./data/members_dic.pkl', 'rb') as pkl_file:
-#     country_dic = pickle.load(pkl_file)
+# Read data from pickled file
+speech_objects = unpickle_list_of_speeches(Path('./data/all_speeches.pickle'))
 
-# Formatted Flight Delay Data for map
-# map_data = pd.read_csv(join(dirname(__file__), 'data', 'flights_map.csv'),
-#                             header=[0,1], index_col=0)
-
-# speech_objects = make_speeches(sample_dataset)
-# pickle_speeches(speech_objects, './data/UN')
-
-
-speech_objects = unpickle_speeches(Path('./data/UN'))
-print('loaded speeches')
-
-# # # Create each of the tabs
+# Create each of the tabs
 tab1 = speech_tab(speech_objects)
-
 tab2 = country_tab(speech_objects)
 tab3 = text_tab(speech_objects)
-# tab4 = map_tab(speech_objects)
-# tab5 = route_tab(flights)
 
-# create_dict(list_of_sp_obj)
 # Put all the tabs into one application
-tabs = Tabs(tabs=[tab1, tab2, tab3])  # , tab4])#, tab2, tab3, tab4, tab5])
+tabs = Tabs(tabs=[tab1, tab2, tab3])
 
 # Put the tabs in the current document for display
 curdoc().theme = 'light_minimal'
