adbbd5a8f44a52ae4da54665e9e35e685cea1090
JelsB <boulangier.jels@gmail.com>
2019-05-19 20:28:34 +0200

    Auto PEP8

--- vizapp/models/map_tab.py
+++ vizapp/models/map_tab.py
@@ -1,13 +1,14 @@
 import bokeh.plotting as bkp
 import bokeh.models as bkm
 
-def make_map():
 
+def make_map():
 
     with open("./countries.geo.json", "r") as f:
         countries = bkm.GeoJSONDataSource(geojson=f.read())
 
-    p = bkp.figure(width=1000, height=600, tools=tools, title='World Countries', x_axis_label='Longitude', y_axis_label='Latitude')
+    p = bkp.figure(width=1000, height=600, tools=tools, title='World Countries',
+                   x_axis_label='Longitude', y_axis_label='Latitude')
     p.background_fill_color = "aqua"
     p.x_range = bkm.Range1d(start=-180, end=180)
     p.y_range = bkm.Range1d(start=-90, end=90)
@@ -16,6 +17,6 @@ def make_map():
     layout = row(p)
 
     # Make a tab with the layout
-    tab = Panel(child=layout, title = 'Word frequency')
+    tab = Panel(child=layout, title='Word frequency')
 
     return tab
